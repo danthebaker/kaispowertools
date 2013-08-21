91231a97b437b07b06cd933429fb96ac2cb0f414
Kai Davenport <kaiyadavenport@gmail.com>
2013-08-21 19:09:15 +0000

    added extra git features

--- scripts/install_developer.sh
+++ scripts/install_developer.sh
@@ -2,3 +2,5 @@
 git config --global user.name "Kai Davenport"
 git config --global user.email "kaiyadavenport@gmail.com"
 git config --global credential.helper 'cache --timeout=3600'
+git config --global push.default simple
+git config --global alias.add-commit '!git add -A && git commit'
