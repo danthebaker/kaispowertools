078229d487a0f1b2c77d09e790dc2b13e2b50e9a
Kai Davenport <kaiyadavenport@gmail.com>
2013-10-13 21:23:28 +0000

    added module command to links script that loops node_modules for modules in project folder and symlinks those that are found

--- scripts/links/index.js
+++ scripts/links/index.js
@@ -45,6 +45,41 @@ program
   })
 
 program
+  .command('nodemodule [module] [projects]')
+  .description('symlink all modules that are projects in the given folder')
+  .action(function(module, projects){
+
+    module = module.replace(/\/$/, '');
+    projects = projects.replace(/\/$/, '');
+
+    var modules_dir = module + '/node_modules';
+
+    fs.readdir(modules_dir, function(error, names){
+      
+      names.forEach(function(name){
+        
+        var exists = fs.existsSync(projects + '/' + name);
+
+        if(exists){
+          var stat = fs.lstatSync(modules_dir + '/' + name);
+          if(stat.isSymbolicLink()){
+            
+          }
+          else{
+            wrench.rmdirSyncRecursive(modules_dir + '/' + name, true);
+            fs.symlinkSync(projects + '/' + name, modules_dir + '/' + name);
+            console.log(projects + '/' + name + ' -> ' + modules_dir + '/' + name);
+          }
+        }
+      })
+
+      console.log('done');
+      
+    })
+    
+  })
+
+program
   .command('*')
   .action(function(command){
     console.log('command: "%s" not found', command);
